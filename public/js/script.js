f3f2bdc2057e17848eaac39f0100fc37d9964502
Isak410 <113894543+Isak410@users.noreply.github.com>
2023-10-04 00:47:40 +0200

    Grunnleggende backend
    fortsatt problemer med 'insert-object' i server.js fil

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,62 @@
 const createButton = document.getElementById('createbutton')
 const selectFile = document.getElementById('input4')
 const retField = document.getElementById('retField')
-var fieldNotFilled = false
+
+
+
 
 
 function createPressed() {
     console.log("createButton pressed")
     for (let i = 1; i < 5; i++) {
         if (document.getElementById('input'+i).value == "") {
-            fieldNotFilled = true
             retField.textContent = "All Fields must be filled"
-            return;
-        }}
-        console.log(typeof(selectFile.value))
-    
+            return;}}
+        if (selectFile.files[0].type != "audio/mpeg") {
+            retField.textContent = "Filetype must be mp3"; return;}
+    retField.textContent = ""
 
+    var arr = []
+    var audioUrl = URL.createObjectURL(selectFile.files[0])
+    for (let t = 1; t < 4; t++ ) {arr[t-1] = (document.getElementById('input'+t).value)}
+    var songObj = {
+        "name": arr[0],
+        "artist": arr[1],
+        "album": arr[2],
+        "audiofile": audioUrl
+    }
+
+    
+    console.log(audioUrl)
+    //console.log(songObj)
+    //console.log(songObj.audiofile)
+    //playAudio(songObj) //test om filen er lastet opp korrekt
+
+    
+    fetch('/insert-object', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(songObj), // Send the data to insert in the request body
+      })
+        .then(response => {
+          response.json()
+          console.log("Data inserted successfully")
+        })
+        .then(data => {
+          console.log(data.message)
+        })
+        .catch(error => {
+          console.log('Error: ' + error);
+        });
+}
+
+function playAudio(songObj) {
+    const audioUrl = URL.createObjectURL(songObj.audiofile)
+
+    const audio = new Audio(audioUrl);
+    audio.play();
 }
 
 
--- server.js
+++ server.js
@@ -1,12 +1,39 @@
 const express = require('express')
-const cors = require('cors')
+const sql = require('msnodesqlv8')
 const app = express();
 const port = 8080
 
+var config = {
+    server: "DESKTOP-N9GI0V9\\SQLEXPRESS",
+    database: "fdb",
+    driver: "msnodesqlv8",
+    options: {
+      trustedConnection: true,
+    },
+  };
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
   });
+
+  app.post('/insert-object', (req, res) => {
+    const { name, artist, album, audiofile } = req.body;
+    console.log(audiofile)
+    sql.connect(config, function (err) {
+        if (err) console.log(err);
+        else {
+            var request = new sql.Request();
+            request.query("INSERT INTO songs (Song_Name, Artist, Album, Sang_Fil) VALUES ('"+name+"','"+artist+"','"+album+"','"+audiofile+"')"), function (err, result) {
+                if (err) console.log(err);
+                else {
+                    res.json("successfully inserted file / info")
+                }
+            };
+        }
+    });
+});   
 
 app.use(express.static('public'));
 
