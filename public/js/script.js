e2fb8df0f15db92c2fe245474962990b23e03de1
Isak410 <113894543+Isak410@users.noreply.github.com>
2023-10-05 01:58:20 +0200

    bare frontend igjen

--- public/js/script.js
+++ public/js/script.js
@@ -1,46 +1,68 @@
 const createButton = document.getElementById('createbutton')
 const selectFile = document.getElementById('input4')
 const retField = document.getElementById('retField')
+const form = document.querySelector('form')
+const nameField = document.getElementById('input1')
+const ArtistField = document.getElementById('input2')
+const AlbumField = document.getElementById('input3')
+const loadButton = document.getElementById('loadSongs')
 
 
 
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log("createButton pressed")
+  for (let i = 1; i < 5; i++) {
+      if (document.getElementById('input'+i).value == "") {
+          retField.textContent = "All Fields must be filled"
+          return;}}
+      if (selectFile.files[0].type != "audio/mpeg") {
+          retField.textContent = "Filetype must be mp3"; return;}
+  retField.textContent = ""
 
+  
+  const formData = new FormData()
+  formData.append("song_name", nameField.value)
+  formData.append("artist", ArtistField.value)
+  formData.append("album", AlbumField.value)
+  formData.append("file", selectFile.files[0]);
 
-function createPressed() {
-    console.log("createButton pressed")
-    for (let i = 1; i < 5; i++) {
-        if (document.getElementById('input'+i).value == "") {
-            retField.textContent = "All Fields must be filled"
-            return;}}
-        if (selectFile.files[0].type != "audio/mpeg") {
-            retField.textContent = "Filetype must be mp3"; return;}
-    retField.textContent = ""
-
-    const form = document.getElementById('uploadForm')
-    const formData = new FormData(form)
-    
-    fetch('/insert-object', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: formData, // Send the data to insert in the request body
-      })
-      .then(response => response.text())
-      .then(message => {
-        console.log(message);
-        // Handle success (e.g., show a success message to the user)
-      })
-      .catch(error => {
-        console.error('Error uploading file:', error.message);
-        // Handle error (e.g., show an error message to the user)
-      });
-}
+  console.log(...formData)
+  
+  fetch('http://127.0.0.1:8080/uploads', {
+    method: 'POST',
+    body: formData,
+  })
+  .then(res => res.json())
+  .then(data => console.log(data))
+});
 
 function playAudio(songObj) {
     const audio = new Audio(songObj.audiofile);
     audio.play();
 }
 
+function loadSongs() {
+  var objectFromFetch = {}
+  fetch('http://127.0.0.1:8080/get-songs')
+  .then(res => res.json())
+  .then (data => {
+    const parsedData = data
+    playSpecificSong(parsedData)
+    displaySongs(parsedData)
+  })
+}
 
-createButton.addEventListener('click', createPressed)
+function playSpecificSong(parsedData) {
+  console.log(parsedData.songInfo[1].filepath);
+  const urlFilePath = parsedData.songInfo[document.getElementById('numberInput').value].filepath;
+  const audio = new Audio(urlFilePath);
+  audio.play();
+}
+
+function displaySongs(parsedData) {
+  console.log(parsedData)
+}
+
+
+loadButton.addEventListener('click', loadSongs)
--- server.js
+++ server.js
@@ -1,36 +1,90 @@
 const express = require('express')
 const app = express();
-const port = 8080
 const multer = require('multer')
+const cors = require('cors')
+const port = 8080
+const fs = require('fs');
+const dir = './public/Audiofiles';
+
+function dirLength() {
+fs.readdir(dir, (err, files) => {return(files.length)});
+}
+
+
+
 
 
 app.use(express.json());
+app.use(cors()) // tillater forespørsler fra hvilken som helst ip
+
+const storage = multer.diskStorage({
+  destination: function(req, file, callback) {
+    callback(null, __dirname + '/public/AudioFiles');
+  },
+  filename: function(req, file, callback) {
+    callback(null, (file.originalname))
+  }
+})
+
+const uploads = multer({storage: storage})
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
   });
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'public/AudioFiles')
-  },
-  filename: (req, file, cn) => {
-    cb(null, file.originalname)
-  },
+app.get("/get-songs", (req, res) => {
+    fs.readFile(__dirname + '/public/AudioFiles/objects.json', 'utf-8', function(err,data){
+    if(err)console.log(err)
+    else{
+    res.json(JSON.parse(data))
+    }
+  })
+})
+
+app.post("/uploads", uploads.array("file"), (req, res) => {
+  console.log(req.body)
+  fs.readdir(dir, (err, files) => {console.log(files.length)});
+  
+var dataFromJSONFile = {};
+
+fs.readFile(__dirname + '/public/AudioFiles/objects.json', 'utf8', function(err, data){
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  try {
+    dataFromJSONFile = JSON.parse(data);
+
+    var myObj = {
+      "song_name": req.body.song_name,
+      "artist": req.body.artist,
+      "album": req.body.album,
+      "filepath": ('../AudioFiles/' + req.files[0].filename)
+    }
+
+    if (!Array.isArray(dataFromJSONFile.songInfo)) {
+      dataFromJSONFile.songInfo = [];
+    }
+
+    dataFromJSONFile.songInfo.push(myObj);
+
+    fs.writeFile(__dirname + '/public/AudioFiles/objects.json', JSON.stringify(dataFromJSONFile), function (err){
+      if(err) console.log(err);
+      else console.log("Successfully wrote file");
+    });
+
+    res.json({status: "files received"});
+  } catch (error) {
+    console.error("Error parsing JSON:", error);
+    res.status(500).json({error: "Internal Server Error"});
+  }
 });
-
-const upload = multer({storage})
-
-  app.post('/insert-object', upload.single('mp3File'), (req, res) => {
-    console.log(req.body)
-
-    //const { name, artist, album, audiofile } = req.body;
-    //console.log(audiofile)
-    
-});   
-
+})
 app.use(express.static('public'));
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-});
+})
