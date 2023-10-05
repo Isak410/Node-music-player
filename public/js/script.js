8c3efcd03efac8da191eaea11d10f2efa9b80255
Isak410 <Isakstenep@gmail.com>
2023-10-05 11:41:49 +0200

    Display songs function fixed

--- public/js/script.js
+++ public/js/script.js
@@ -6,7 +6,7 @@ const nameField = document.getElementById('input1')
 const ArtistField = document.getElementById('input2')
 const AlbumField = document.getElementById('input3')
 const loadButton = document.getElementById('loadSongs')
-
+const songDiv = document.getElementById('displaySongDiv')
 
 
 form.addEventListener('submit', (e) => {
@@ -18,6 +18,8 @@ form.addEventListener('submit', (e) => {
           return;}}
       if (selectFile.files[0].type != "audio/mpeg") {
           retField.textContent = "Filetype must be mp3"; return;}
+      var fileName = selectFile.files[0].name
+      if (fileName.includes(' ')){retField.innerHTML = "filename must not contain spaces"; return;}
   retField.textContent = ""
 
   
@@ -43,7 +45,6 @@ function playAudio(songObj) {
 }
 
 function loadSongs() {
-  var objectFromFetch = {}
   fetch('http://127.0.0.1:8080/get-songs')
   .then(res => res.json())
   .then (data => {
@@ -54,14 +55,35 @@ function loadSongs() {
 }
 
 function playSpecificSong(parsedData) {
-  console.log(parsedData.songInfo[1].filepath);
-  const urlFilePath = parsedData.songInfo[document.getElementById('numberInput').value].filepath;
+  console.log(parsedData.songInfo[0].filepath);
+  const urlFilePath = parsedData.songInfo[0].filepath;
   const audio = new Audio(urlFilePath);
   audio.play();
 }
 
 function displaySongs(parsedData) {
   console.log(parsedData)
+
+  for (let i = 0; i < parsedData.songInfo.length; i++) {
+    var navn = document.createTextNode(parsedData.songInfo[i].song_name)
+    var artist = document.createTextNode(parsedData.songInfo[i].artist)
+    var album = document.createTextNode(parsedData.songInfo[i].album)
+    var audioplayer = document.createElement("audio")
+    audioplayer.src = parsedData.songInfo[i].filepath
+    audioplayer.type="audio/mp3"
+    audioplayer.controls = true
+    var newDiv = document.createElement("div")
+    newDiv.id = "newDiv"
+    newDiv.appendChild(navn)
+    newDiv.appendChild(document.createElement("br"))
+    newDiv.appendChild(artist)
+    newDiv.appendChild(document.createElement("br"))
+    newDiv.appendChild(album)
+    newDiv.appendChild(document.createElement("br"))
+    newDiv.appendChild(audioplayer)
+    console.log(newDiv)
+    songDiv.appendChild(newDiv)
+  }
 }
 
 
